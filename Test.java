4f78258816748ee10b1cb5ba8e3c12a26d73e1ce
toerry <patrikdue_2@hotmail.com>
2013-11-03 13:14:45 +0100

    shit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+	public static void main(String[] args) {
+	String name = "cs107";
+	if (args.length > 0) {
+	name = args[0];
+	}
+	System.out.printf("Hello␣%s\n", name);
+	}
+}
+
